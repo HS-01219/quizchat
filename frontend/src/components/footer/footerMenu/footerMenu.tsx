235d349d01b6efe5b3380ca395192d97358a0fcf
heeyoung123 <heeyoung321@naver.com>
2025-05-25 15:10:48 +0900

    feat : 아이콘 페이지 연결 추가

--- frontend/src/components/footer/footerMenu/footerMenu.tsx
+++ frontend/src/components/footer/footerMenu/footerMenu.tsx
@@ -2,23 +2,26 @@ import { FiUser } from "react-icons/fi";
 import { RiFileListLine } from "react-icons/ri";
 import * as S from "@/components/footer/footerMenu/footerMenu.style";
 import {LABEL} from "@/constants/Label";
-
+import {useNavigate} from "react-router-dom";
 const FooterMenu = () => {
+	const nav=useNavigate();
+	const handleVoteClick = () => {
+		nav("/vote");
+	}
+	const handleQuizClick = () => {
+	nav("/quiz");
+	}
 	return (
 <S.FooterMenuContainer>
-	<S.FooterMenuWrapper>	<RiFileListLine/>
+	<S.FooterMenuWrapper>	<RiFileListLine onClick={handleVoteClick} />
 		<S.FooterMenuLabel>	{LABEL.VOTE}</S.FooterMenuLabel>
 	</S.FooterMenuWrapper>
 <S.FooterMenuWrapper>
-	<FiUser/>
+	<FiUser onClick={handleQuizClick} />
 	<S.FooterMenuLabel>
 		{LABEL.QUIZ}
 	</S.FooterMenuLabel>
 </S.FooterMenuWrapper>
-
-
-
-
 </S.FooterMenuContainer>
 	);
 };
