fc899525254ae69abd2f4173aa05e2c0fc4e0b26
HS <mcm009@naver.com>
2025-06-12 00:27:28 +0900

    fix : 퀴즈 진행 오류 수정
    백엔드 퀴즈 조건문 오류 수정
    프론트 QuizState null일때 isActive 상태 관련 오류 수정

--- backend/socket/quizHandler.ts
+++ backend/socket/quizHandler.ts
@@ -43,82 +43,62 @@ export function handleQuiz(io: Server, socket: Socket) {
     }
   });
 
-  socket.on("ANSWER_QUIZ", async ({ userId, answer }: answerPayload) => {
-    // if절로 퀴즈가 진행되고 있을때만 아래 로직을 수행
-    const currentQuiz = await getCurrentQuizState();
-    if (currentQuiz && currentQuiz.isActive) {
-      if (!currentQuiz.quizData) {
-        return socket.emit("ANSWER_QUIZ_ERROR", {
-          message: "퀴즈 데이터가 없습니다.",
-        });
-      }
-    } else {
-      socket.emit("START_QUIZ_ERROR", { message: "퀴즈가 존재하지 않습니다." });
-      return;
-    }
+    socket.on("ANSWER_QUIZ", async ({ userId, answer }: answerPayload) => {
+        // if절로 퀴즈가 진행되고 있을때만 아래 로직을 수행
+        const currentQuiz = await getCurrentQuizState();
+        if (currentQuiz && currentQuiz.isActive) {
+            if (!currentQuiz.quizData) {
+                return socket.emit("ANSWER_QUIZ_ERROR", { message: "퀴즈 데이터가 없습니다.", });
+            }
 
-    // 서버가 가지고 있는 정답과 일치하는지 체크
-    const isCorrect = currentQuiz.quizData
-      ? checkAnswer(answer, currentQuiz.quizData.answer)
-      : false;
+            // 서버가 가지고 있는 정답과 일치하는지 체크
+            const isCorrect = currentQuiz.quizData ? checkAnswer(answer, currentQuiz.quizData.answer) : false;
+        
+            console.log("퀴즈 정답 체크", isCorrect);
+            
+            // 정답일 경우
+            if (isCorrect) {
+                // redis에 퀴즈 상태를 업데이트
+                const quizState: QuizState = {
+                isActive: false,
+                quizData: null,
+                isEnded: true,
+                };
+        
+                await setRedisValue("quizState", JSON.stringify(quizState), 60 * 60);
+        
+                const winnerId = socket.data.userId;
+                const winnerNickName = socket.data.nickName;
+                const answer = currentQuiz.quizData.answer;
+        
+                // 서버 내부에서 퀴즈 종료 후 클라이언트에게 퀴즈 종료 신호와 정답자, 정답 전달
+                io.emit("END_QUIZ", {
+                    winnerId: winnerId,
+                    winnerNickName: winnerNickName,
+                    answer: answer,
+                });
 
-    console.log("퀴즈 정답 체크", isCorrect);
-    // 정답일 경우
-    if (isCorrect) {
-      // redis에 퀴즈 상태를 업데이트
-      const quizState: QuizState = {
-        isActive: false,
-        quizData: null,
-        isEnded: true,
-      };
+                io.emit("SYSTEM_MESSAGE", {
+                    type: "correct",
+                    nickName: winnerNickName,
+                    time: Date.now(),
+                });
 
-      // 서버가 가지고 있는 정답과 일치하는지 체크
-      const isCorrect = currentQuiz.quizData
-        ? checkAnswer(answer, currentQuiz.quizData.answer)
-        : false;
+                io.emit("SYSTEM_MESSAGE", {
+                    type: "quizEnd",
+                    time: Date.now(),
+                });
 
-      console.log("퀴즈 정답 체크", isCorrect);
-      // 정답일 경우
-      if (isCorrect) {
-        // redis에 퀴즈 상태를 업데이트
-        const quizState: QuizState = {
-          isActive: false,
-          quizData: null,
-          isEnded: true,
-        };
-
-        await setRedisValue("quizState", JSON.stringify(quizState), 60 * 60);
-
-        const winnerId = socket.data.userId;
-        const winnerNickName = socket.data.nickName;
-        const answer = currentQuiz.quizData.answer;
-
-        // 서버 내부에서 퀴즈 종료 후 클라이언트에게 퀴즈 종료 신호와 정답자, 정답 전달
-        io.emit("END_QUIZ", {
-          winnerId: winnerId,
-          winnerNickName: winnerNickName,
-          answer: answer,
-        });
-        io.emit("SYSTEM_MESSAGE", {
-          type: "correct",
-          nickName: winnerNickName,
-          time: Date.now(),
-        });
-        io.emit("SYSTEM_MESSAGE", {
-          type: "quizEnd",
-          time: Date.now(),
-        });
-      } else {
-        console.log("정답 아님");
-        // 별도의 처리 X, 논의 후 수정
-      }
-    } else {
-      socket.emit("ANSWER_QUIZ_ERROR", {
-        message: "퀴즈가 진행중이지 않습니다.",
-      });
-      return;
-    }
-  });
+            } else {
+                console.log("정답 아님");
+                // 별도의 처리 X, 논의 후 수정
+            }
+        } else {
+            return socket.emit("ANSWER_QUIZ_ERROR", {
+                message: "퀴즈가 진행중이지 않습니다.",
+            });
+        }
+    });
 }
 
 const selectQuiz = async (): Promise<QuizItem | null> => {
